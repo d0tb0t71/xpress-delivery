3995f3db3632ce76150501526e7a8c4fa56f0cb4
d0tb0t71 <sdipon.sutradhar@gmail.com>
2022-03-03 19:37:49 +0600

    c-8

--- app/src/main/java/com/example/xpressdelivery/ParcelDetails.java
+++ app/src/main/java/com/example/xpressdelivery/ParcelDetails.java
@@ -1,9 +1,17 @@
 package com.example.xpressdelivery;
 
+import static com.google.android.gms.common.util.CollectionUtils.mapOf;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.google.firebase.firestore.DocumentReference;
@@ -15,8 +23,11 @@ import com.google.firebase.firestore.FirebaseFirestoreException;
 public class ParcelDetails extends AppCompatActivity {
 
 
-    TextView parcelID, product_type, issued_date, receiver, r_mobile, r_address, sender;
+    TextView parcelID, product_type, issued_date, receiver, r_mobile, r_address, sender, r_email;
     FirebaseFirestore db;
+    String selected = "1. Issued";
+    LinearLayout l1, l2, l3, l4, l5;
+    Button update_status_btn;
 
 
     @Override
@@ -35,9 +46,45 @@ public class ParcelDetails extends AppCompatActivity {
         r_mobile = findViewById(R.id.r_mobile);
         r_address = findViewById(R.id.r_address);
         sender = findViewById(R.id.sender);
+        r_email = findViewById(R.id.r_email);
+
+        update_status_btn = findViewById(R.id.update_status_btn);
+
+
+        l1 = findViewById(R.id.l1);
+        l2 = findViewById(R.id.l2);
+        l3 = findViewById(R.id.l3);
+        l4 = findViewById(R.id.l4);
+        l5 = findViewById(R.id.l5);
 
 
         db = FirebaseFirestore.getInstance();
+
+
+        Spinner dropdown = findViewById(R.id.spinner1);
+        String[] items = new String[]{"1. Issued", "2. Way to Warehouse", "3. In Warehouse", "4. Way to Receiver", "5. Delivered"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        dropdown.setAdapter(adapter);
+
+        dropdown.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> arg0, View arg1,
+                                       int arg2, long arg3) {
+
+                selected = dropdown.getSelectedItem().toString();
+                System.out.println(selected);
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+
+        });
+
+
 
 
         DocumentReference documentReference = db.collection("parcel").document(pID);
@@ -48,13 +95,137 @@ public class ParcelDetails extends AppCompatActivity {
                 parcelID.setText("Parcel ID - \n" + value.getString("pID"));
                 product_type.setText("Product Type : " + value.getString("pType"));
                 issued_date.setText("Issued Date : " + value.getString("issueDate"));
-                receiver.setText("Receiver : " + value.getString("rName")+"  *");
+                receiver.setText("Receiver : " + value.getString("rName") + "  *");
+                r_email.setText("Receiver : " + value.getString("rEmail"));
                 r_mobile.setText("Receiver Mobile : " + value.getString("rMobile"));
-                r_address.setText("Receiver Address : " + value.getString("name"));
-                sender.setText("Sender : " + value.getString("sName")+"  *");
+                r_address.setText("Receiver Address : " + value.getString("rAdd"));
+                sender.setText("Sender : " + value.getString("sName") + "  *");
+
+
+                String issued = value.getString("issued");
+                String wayToWH = value.getString("wayToWH");
+                String inWH = value.getString("inWH");
+                String wayToR = value.getString("wayToR");
+                String delivered = value.getString("delivered");
+
+
+                if (wayToR.equals("true")) {
+                    int spinnerPosition = adapter.getPosition("5. Delivered");
+                    dropdown.setSelection(spinnerPosition);
+                    l5.setVisibility(View.VISIBLE);
+                    l4.setVisibility(View.VISIBLE);
+                    l3.setVisibility(View.VISIBLE);
+                    l2.setVisibility(View.VISIBLE);
+                    l1.setVisibility(View.VISIBLE);
+                    System.out.println("5555555555555555555");
+                } else if (wayToR.equals("true")) {
+                    int spinnerPosition = adapter.getPosition("4. Way to Receiver");
+                    dropdown.setSelection(spinnerPosition);
+                    l4.setVisibility(View.VISIBLE);
+                    l3.setVisibility(View.VISIBLE);
+                    l2.setVisibility(View.VISIBLE);
+                    l1.setVisibility(View.VISIBLE);
+                    System.out.println("4444444444444444444");
+                } else if (inWH.equals("true")) {
+                    int spinnerPosition = adapter.getPosition("3. In Warehouse");
+                    dropdown.setSelection(spinnerPosition);
+                    l3.setVisibility(View.VISIBLE);
+                    l2.setVisibility(View.VISIBLE);
+                    l1.setVisibility(View.VISIBLE);
+                    System.out.println("333333333333333333");
+                } else if (wayToWH.equals("true")) {
+                    int spinnerPosition = adapter.getPosition("2. Way to Warehouse");
+                    dropdown.setSelection(spinnerPosition);
+                    l2.setVisibility(View.VISIBLE);
+                    l1.setVisibility(View.VISIBLE);
+                    System.out.println("222222222222222222222");
+                } else if (issued.equals("true")) {
+                    int spinnerPosition = adapter.getPosition("1. Issued");
+                    dropdown.setSelection(spinnerPosition);
+                    l1.setVisibility(View.VISIBLE);
+                    System.out.println("111111111111111111");
+                }
+
+
+            }
+        });
+
+
+        update_status_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                if (selected.equals("5. Delivered")) {
+
+                    db.collection("parcel")
+                            .document(pID)
+                            .update(
+                                    "delivered","true",
+                                    "wayToR","true",
+                                    "inWH","true",
+                                    "wayToWH","true",
+                                    "issued","true"
+                            );
+
+                }
+                else if (selected.equals("4. Way to Receiver")) {
+
+                    db.collection("parcel")
+                            .document(pID)
+                            .update(
+                                    "delivered","false",
+                                    "wayToR","true",
+                                    "inWH","true",
+                                    "wayToWH","true",
+                                    "issued","true"
+                            );
+
+                }
+                else if (selected.equals("3. In Warehouse")) {
+
+                    db.collection("parcel")
+                            .document(pID)
+                            .update(
+                                    "delivered","false",
+                                    "wayToR","false",
+                                    "inWH","true",
+                                    "wayToWH","true",
+                                    "issued","true"
+                            );
+
+                }
+                else if (selected.equals("2. Way to Warehouse")) {
+
+                    db.collection("parcel")
+                            .document(pID)
+                            .update(
+                                    "delivered","false",
+                                    "wayToR","false",
+                                    "inWH","false",
+                                    "wayToWH","true",
+                                    "issued","true"
+                            );
+
+                }
+                else if (selected.equals("1. Issued")) {
+
+                    db.collection("parcel")
+                            .document(pID)
+                            .update(
+                                    "delivered","false",
+                                    "wayToR","false",
+                                    "inWH","false",
+                                    "wayToWH","false",
+                                    "issued","true"
+                            );
+
+                }
 
 
 
+
+                recreate();
             }
         });
 
--- app/src/main/java/com/example/xpressdelivery/TrackWithIDFragment.java
+++ app/src/main/java/com/example/xpressdelivery/TrackWithIDFragment.java
@@ -1,23 +1,82 @@
 package com.example.xpressdelivery;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
 
 public class TrackWithIDFragment extends Fragment {
 
    View view;
+   EditText track_id;
+   Button track_btn;
+    FirebaseFirestore db;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_track_with_i_d, container, false);
+
+        track_id = view.findViewById(R.id.track_id);
+        track_btn = view.findViewById(R.id.track_btn);
+
+        db = FirebaseFirestore.getInstance();
+
+        track_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(track_id.getText().toString()!="" && track_id.getText().toString().contains("XD")){
+
+
+                    DocumentReference docIdRef = db.collection("parcel").document(track_id.getText().toString());
+                    docIdRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                            if (task.isSuccessful()) {
+                                DocumentSnapshot document = task.getResult();
+                                if (document.exists()) {
+                                    Intent intent = new Intent(getContext(),ParcelDetails.class);
+                                    intent.putExtra("pID",track_id.getText().toString());
+                                    startActivity(intent);
+                                } else {
+                                    track_id.setError("Enter An Valid ID");
+                                }
+                            } else {
+                                track_id.setError("Enter An Valid ID");
+                            }
+                        }
+                    });
+
+
+                }
+                else{
+                   // Toast.makeText(getActivity(), "Enter An Valid ID", Toast.LENGTH_SHORT).show();
+                    track_id.setError("Enter An Valid ID");
+                }
+
+            }
+        });
+
         return view;
     }
 }
